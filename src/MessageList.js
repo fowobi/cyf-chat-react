24304855489930d85f8478addfef71d0fa5b4e4b
fowobi <109865505+fowobi@users.noreply.github.com>
2023-07-21 17:13:05 +0100

    Update MessageList.js

--- src/MessageList.js
+++ src/MessageList.js
@@ -3,25 +3,40 @@ import axios from "axios";
 
 const MessageList = () => {
   const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState({ from: "", text: "" });
+  const [loading, setLoading] = useState(true);
 
-  // Function to fetch the latest messages from the server
+  // const fetchLatestMessages = () => {
+  //   axios
+  //     .get("https://tosin-chat-server.glitch.me/messages/latest")
+  //     .then((response) => {
+  //       setMessages(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching latest messages:", error);
+  //     })
+  //     .finally(() => {
+  //       setLoading(false);
+  //     });
+  // };
+
   const fetchLatestMessages = () => {
     axios
       .get("https://tosin-chat-server.glitch.me/messages/latest")
       .then((response) => {
         setMessages(response.data);
+        setLoading(false); // Set loading to false when data is fetched successfully
       })
       .catch((error) => {
         console.error("Error fetching latest messages:", error);
+        setLoading(false); // Set loading to false on error as well
       });
   };
 
-  // Function to delete a message from the server and local display
   const deleteMessage = (id) => {
     axios
       .delete(`https://tosin-chat-server.glitch.me/messages/${id}`)
       .then(() => {
-        // Remove the deleted message from the local display
         setMessages(messages.filter((msg) => msg.id !== id));
       })
       .catch((error) => {
@@ -29,25 +44,88 @@ const MessageList = () => {
       });
   };
 
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewMessage({ ...newMessage, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Check if the name or message field is empty
+    if (!newMessage.from.trim() || !newMessage.text.trim()) {
+      console.log("Name and message cannot be empty!");
+      return;
+    }
+
+    axios
+      .post("https://tosin-chat-server.glitch.me/messages", newMessage)
+      .then((response) => {
+        fetchLatestMessages(); // Fetch the latest messages again after adding a new one
+        setNewMessage({ from: "", text: "" }); // Clear the form fields
+      })
+      .catch((error) => {
+        console.error("Error sending message:", error);
+      });
+  };
+
   useEffect(() => {
-    // Fetch the latest messages on component mount
     fetchLatestMessages();
   }, []);
 
   return (
     <div>
-      <h2>Latest Messages</h2>
-      <ul>
-        {messages.map((message) => (
-          <li key={message.id}>
-            <span>
-              From: {message.from}, Message: {message.text}
-            </span>
-            <button onClick={() => deleteMessage(message.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-      <button onClick={fetchLatestMessages}>See Latest</button>
+      {/* Show a loading message while fetching data */}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h2>Latest Messages</h2>
+          <ul>
+            {messages.map((message) => (
+              <li key={message.id}>
+                <span>
+                  From: {message.from}, Message: {message.text}
+                </span>
+                <button onClick={() => deleteMessage(message.id)}>
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+          <button onClick={fetchLatestMessages}>See Latest</button>
+        </>
+      )}
+
+      <h2>Send a message</h2>
+      <form onSubmit={handleSubmit}>
+        <p>
+          Name:{" "}
+          <input
+            type="text"
+            name="from"
+            placeholder="Your Name"
+            value={newMessage.from}
+            onChange={handleInputChange}
+          />{" "}
+          <br />
+          Message:{" "}
+          <input
+            type="text"
+            name="text"
+            placeholder="The message..."
+            value={newMessage.text}
+            onChange={handleInputChange}
+          />{" "}
+          <br />
+        </p>
+        <button
+          type="submit"
+          disabled={!newMessage.from.trim() || !newMessage.text.trim()}
+        >
+          Send
+        </button>
+      </form>
     </div>
   );
 };
