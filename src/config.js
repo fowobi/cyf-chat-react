f14c751f11363e77fca05e5eae5e1a6c7a790b2e
fowobi <109865505+fowobi@users.noreply.github.com>
2023-11-16 11:40:55 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // config.js
-const API_URL = "https://tosin-chat-server.glitch.me"; // Replace with your actual API URL
+const API_URL = "https://tosin-chat-server.glitch.me" || "http://localhost:18080/messages";
 
 export default API_URL;
